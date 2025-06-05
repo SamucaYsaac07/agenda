e1ea2af7c0688275489bd5bdb42fccbf5d6a061e
SamucaYsaac07 <jogandorsamucaysaac@gmail.com>
2025-06-05 11:15:24 -0300

    implementado adicionar compromisso e mostrar os detalhes...

--- index/urls.py
+++ index/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
 from . import views
 
-urlpatterns = {
+urlpatterns = [
     path('', views.index, name='index'),
     path('compromisso', views.mostrar_compromisso, name='motrar_compromisso'),
     path('marcar', views.marcar, name='marcar')
-}
+]
--- index/views.py
+++ index/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib import messages
 from .models import compromisso
 
@@ -18,11 +18,18 @@ def marcar(request):
 
         atual = compromisso.objects.create(titulo=titulo,descricao=descricao,data=data)
         atual.save()
-        return redirect(request, "/")
+
+        return redirect("/")
+
+        # return redirect(request, "/")
 
     else:
         return render(request, "marcar.html")
 
 def mostrar_compromisso(request, id):
     atual = compromisso.objects.query(id=id)
+    # atual = compromisso.objects.query(id=id)
+    # atual = compromisso.objects.get(id=id)
+    atual = get_object_or_404(compromisso, id=id)
+
     return render(request, "mostrar_compromisso.html", {"compromisso":atual})
